2948f97fd01788a7e809a751f31911e9aaaf185e
Justin <justinbaradi@hotmail.com>
2013-03-20 15:32:48 -0400

    Another feature

--- PasswordTestor/src/mainpackage/Main.java
+++ PasswordTestor/src/mainpackage/Main.java
@@ -10,5 +10,7 @@ public class Main {
 		String pass = scan.nextLine();
 		if (pass.length() == 0)
 			System.out.println("At least enter a character, COME ON!");
+		else if (pass.length() == 1)
+			System.out.println("Finally, a character!");
 	}
 }
